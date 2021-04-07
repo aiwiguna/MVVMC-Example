6cb39700fae4f2d7ebaf4424a81967062ad99699
Antoni Wiguna <antoni.wiguna@gmail.com>
2021-04-07 12:09:19 +0700

    Initial Commit

---
+++ MVVMC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMC
+//
+//  Created by Antoni Wiguna on 07/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
